431fb2557281b91024cc61d8d11dc4bda9f4a50d
Ferendo <dylanfrendo@gmail.com>
2019-01-24 22:56:06 +0100

    Added Google API key and config file for logging

--- Data Gather/GetDataSetFromYoutube.py
+++ Data Gather/GetDataSetFromYoutube.py
@@ -1,9 +1,16 @@
 import configparser
 import os
+import logging
+
+# Logging
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
 
 currentPath = os.path.dirname(os.path.realpath(__file__))
 config = configparser.ConfigParser()
 config.sections()
 config.read(currentPath + '\\..\\config.ini')
 
-print(config['DataGather']['YoutubeChannelId'])
+print(config['General']['YoutubeChannelId'])
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Skipperino-Kripperino-Machine-Learino
+
+pip install --upgrade google-api-python-client
+pip install --upgrade google-auth google-auth-oauthlib google-auth-httplib2
