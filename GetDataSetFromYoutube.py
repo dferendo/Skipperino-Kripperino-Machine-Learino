956aacd6852c987299501a9217ec6f00eda25bbf
Ferendo <dylanfrendo@gmail.com>
2019-01-27 16:03:12 +0100

    Updated script to print all of the channel video Ids

--- GetDataSetFromYoutube.py
+++ GetDataSetFromYoutube.py
@@ -1,6 +1,15 @@
 from googleapiclient.discovery import build
-import json
 import logging
+import json
+
+
+class Video:
+    video_id = ""
+    video_published_at = ""
+
+    def __init__(self, video_id, video_published_at):
+        self.video_id = video_id
+        self.video_published_at = video_published_at
 
 
 API_SERVICE_NAME = 'youtube'
@@ -21,5 +30,31 @@ channel_parameters = {
 channel_info = client.channels().list(**channel_parameters).execute()
 playlist_id = channel_info['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
+videos = []
+pageToken = ""
+playlist_parameters = {
+    'part': 'contentDetails',
+    'playlistId': playlist_id,
+    'maxResults': 50
+}
 
-print(playlist_id)
+while True:
+    playlist_parameters['pageToken'] = pageToken
+    uploaded_videos = client.playlistItems().list(**playlist_parameters).execute()
+
+    for video in uploaded_videos['items']:
+        content_details_video = video['contentDetails']
+
+        videos.append(Video(content_details_video['videoId'], content_details_video['videoPublishedAt']))
+
+    if 'nextPageToken' in uploaded_videos:
+        next_page_token = uploaded_videos['nextPageToken']
+        pageToken = next_page_token
+
+        if next_page_token == "":
+            break
+    else:
+        break
+
+with open('./DataSet/videos.json', 'w') as out_file:
+    json.dump([video.__dict__ for video in videos], out_file)
