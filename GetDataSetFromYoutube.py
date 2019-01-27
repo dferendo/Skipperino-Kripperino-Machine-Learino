b997f081076b3c0720b2943b7e002f3d37a609d8
Ferendo <dylanfrendo@gmail.com>
2019-01-27 19:15:01 +0100

    Added convertion from json to object for python

--- GetDataSetFromYoutube.py
+++ GetDataSetFromYoutube.py
@@ -6,19 +6,34 @@ import json
 class Video:
     video_id = ""
     video_published_at = ""
+    game_starting_time = ""
 
-    def __init__(self, video_id, video_published_at):
+    def __init__(self, video_id, video_published_at, game_starting_time):
         self.video_id = video_id
         self.video_published_at = video_published_at
+        self.game_starting_time = game_starting_time
 
 
 API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
 API_KEY = 'AIzaSyDBlDO4a5P8oIUENzgS5VPC5lsPrV5Aam0'
 CHANNEL_ID = 'UCeBMccz-PDZf6OB4aV6a3eA'
+VIDEOS_DATA_LOCATION = './DataSet/videos.json'
+TRACK_COMMENTS_ACCOUNT_ID = 'UCsuOjz5JNLAPN4qUMeTUguQ'
 
 logging.basicConfig(filename="logging.log", format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
                     level=logging.DEBUG)
+
+
+def convert_json_to_object(videos_file):
+    videos_in_json = json.load(videos_file)
+    videos = []
+
+    for video_in_json in videos_in_json:
+        videos.append(Video(video_in_json['video_id'], video_in_json['video_published_at'],
+                            video_in_json['game_starting_time']))
+
+    return videos
 
 
 def get_videos_ids():
@@ -45,7 +60,7 @@ def get_videos_ids():
         for video in uploaded_videos['items']:
             content_details_video = video['contentDetails']
 
-            videos.append(Video(content_details_video['videoId'], content_details_video['videoPublishedAt']))
+            videos.append(Video(content_details_video['videoId'], content_details_video['videoPublishedAt'], ""))
 
         if 'nextPageToken' in uploaded_videos:
             next_page_token = uploaded_videos['nextPageToken']
@@ -56,10 +71,33 @@ def get_videos_ids():
         else:
             break
 
-    with open('./DataSet/videos.json', 'w') as out_file:
+    with open(VIDEOS_DATA_LOCATION, 'w') as out_file:
         json.dump([video.__dict__ for video in videos], out_file)
 
 
-client = build(API_SERVICE_NAME, API_VERSION, developerKey=API_KEY)
-# get_videos_ids()
+def get_game_starting_time_from_comments():
 
+    with open(VIDEOS_DATA_LOCATION) as videos_file:
+        videos = convert_json_to_object(videos_file)
+
+        comment_thread_parameters = {
+            'part': 'snippet',
+            'channelId': TRACK_COMMENTS_ACCOUNT_ID,
+            'maxResults': 20
+        }
+
+        for video in videos:
+            comment_thread_parameters['videoId'] = video.video_id
+
+            comments_in_video = client.commentThreads().list(**comment_thread_parameters).execute()
+
+            break
+
+        print(len(videos))
+
+
+client = build(API_SERVICE_NAME, API_VERSION, developerKey=API_KEY)
+
+if __name__ == "__main__":
+    # get_videos_ids()
+    get_game_starting_time_from_comments()
