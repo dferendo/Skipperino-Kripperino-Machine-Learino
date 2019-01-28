f74519546a589b4301c33e80fb81c45ed4230930
Ferendo <dylanfrendo@gmail.com>
2019-01-28 23:10:42 +0100

    Script will stop searching for comments once the quota is exceeded and will dump the obtained data to a json file.

--- GatherDataSetScripts/GetGameStartingTime.py
+++ GatherDataSetScripts/GetGameStartingTime.py
@@ -1,6 +1,14 @@
 import GatherDataSetScripts.Video as VideoClass
 import re
 import json
+from googleapiclient.errors import HttpError
+
+
+def dump_file(data_set_location, videos):
+    with open(data_set_location, 'w+') as videos_file:
+        # Clear the json file and dump
+        videos_file.truncate(0)
+        json.dump([video.__dict__ for video in videos], videos_file)
 
 
 def get_game_starting_time_from_comments(client, data_set_location, comment_tracker):
@@ -16,40 +24,47 @@ def get_game_starting_time_from_comments(client, data_set_location, comment_trac
         }
 
         for video in videos:
-            if video.is_game_starting_time_checked:
-                continue
+            try:
+                if video.is_game_starting_time_checked:
+                    continue
 
-            comment_thread_parameters['videoId'] = video.video_id
+                comment_thread_parameters['videoId'] = video.video_id
 
-            while True:
-                comment_thread_parameters['pageToken'] = page_token
+                while True:
+                    comment_thread_parameters['pageToken'] = page_token
 
-                comments_in_video = client.commentThreads().list(**comment_thread_parameters).execute()
+                    comments_in_video = client.commentThreads().list(**comment_thread_parameters).execute()
 
-                for comment_thread in comments_in_video['items']:
-                    author = comment_thread['snippet']['topLevelComment']['snippet']['authorChannelId']['value']
+                    for comment_thread in comments_in_video['items']:
+                        author = comment_thread['snippet']['topLevelComment']['snippet']['authorChannelId']['value']
 
-                    # Comment found, save it
-                    if comment_tracker == author:
-                        comment = comment_thread['snippet']['topLevelComment']['snippet']['textOriginal']
-                        video.game_starting_time = re.findall(time_stamp_regex, comment)
+                        # Comment found, save it
+                        if comment_tracker == author:
+                            comment = comment_thread['snippet']['topLevelComment']['snippet']['textOriginal']
+                            video.game_starting_time = re.findall(time_stamp_regex, comment)
+                            video.is_game_starting_time_checked = True
+                            break
+
+                    # If we found the comment, stop looking (Assumption the user posts once, he usually does post once)
+                    if video.is_game_starting_time_checked:
+                        break
+
+                    if 'nextPageToken' in comments_in_video:
+                        next_page_token = comments_in_video['nextPageToken']
+                        page_token = next_page_token
+
+                        if next_page_token == "":
+                            video.is_game_starting_time_checked = True
+                            break
+                    else:
                         video.is_game_starting_time_checked = True
                         break
 
-                # If we found the comment, stop looking (Assumption the user posts once, he usually does post once)
-                if video.is_game_starting_time_checked:
-                    break
-
-                if 'nextPageToken' in comments_in_video:
-                    next_page_token = comments_in_video['nextPageToken']
-                    page_token = next_page_token
-
-                    if next_page_token == "":
+            except HttpError as error:
+                if error.resp.get('content-type', '').startswith('application/json'):
+                    reason = json.loads(error.content).get('error').get('errors')[0].get('reason')
+                    if reason == 'quotaExceeded' or reason == 'dailyLimitExceeded':
+                        dump_file(data_set_location, videos)
                         break
-                else:
-                    break
 
-    with open(data_set_location, 'w+') as videos_file:
-        # Clear the json file and dump
-        videos_file.truncate(0)
-        json.dump([video.__dict__ for video in videos], videos_file)
+    dump_file(data_set_location, videos)
