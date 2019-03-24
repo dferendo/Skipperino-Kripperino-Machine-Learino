2a31533efeb0aff7c9203040ce131adafaa1de4c
Ferendo <dylanfrendo@gmail.com>
2019-03-24 19:22:05 +0100

    Added code to upload comment

--- DetectNewVideosAndTimestampThem/DetectNewVideoUpload.py
+++ DetectNewVideosAndTimestampThem/DetectNewVideoUpload.py
@@ -1,7 +1,7 @@
 import sched
 import time
 import logging
-import DetectNewVideosAndTimestampThem.MakeTimestampOnNewVideo as make_timestamp
+import DetectNewVideosAndTimestampThem.MakeTimestampOnNewVideo as MakeTimestamp
 
 scheduler = None
 delay = 0
@@ -13,17 +13,18 @@ def init_scheduler(api_client, scheduler_delay, channel_id, new_videos_location)
     delay = scheduler_delay
     playlist_id = get_playlist_of_channel(api_client, channel_id)
 
-    scheduler.enter(delay, 1, check_if_there_is_new_video_upload, (api_client, None, playlist_id, new_videos_location))
+    scheduler.enter(delay, 1, check_if_there_is_new_video_upload, (api_client, channel_id, None, playlist_id,
+                                                                   new_videos_location))
     scheduler.run()
 
 
-def check_if_there_is_new_video_upload(api_client, last_video_id, playlist_id, new_videos_location):
+def check_if_there_is_new_video_upload(api_client, channel_id, last_video_id, playlist_id, new_videos_location):
     logging.info("Checking if new video was uploaded recently.")
 
     # Get the latest video on first call
     current_last_video_id = get_latest_video_from_channel(api_client, playlist_id)
     # TODO: Remove
-    make_timestamp.make_timestamp_on_new_video(current_last_video_id, new_videos_location)
+    MakeTimestamp.make_timestamp_on_new_video(api_client, channel_id, current_last_video_id, new_videos_location)
 
     '''
     if last_video_id is None:
@@ -34,7 +35,7 @@ def check_if_there_is_new_video_upload(api_client, last_video_id, playlist_id, n
         last_video_id = current_last_video_id
     '''
 
-    scheduler.enter(delay, 1, check_if_there_is_new_video_upload, (api_client, last_video_id, playlist_id,
+    scheduler.enter(delay, 1, check_if_there_is_new_video_upload, (api_client, channel_id, last_video_id, playlist_id,
                                                                    new_videos_location))
 
 
--- DetectNewVideosAndTimestampThem/MakeTimestampOnNewVideo.py
+++ DetectNewVideosAndTimestampThem/MakeTimestampOnNewVideo.py
@@ -3,7 +3,7 @@ import tempfile
 import os
 
 
-def make_timestamp_on_new_video(video_id, new_videos_location):
+def make_timestamp_on_new_video(api_client, channel_id, video_id, new_videos_location):
     frames_per_second = 0.5
     video_location = get_and_download_video_local_location(video_id, new_videos_location)
 
@@ -13,7 +13,17 @@ def make_timestamp_on_new_video(video_id, new_videos_location):
 
         for filename in os.listdir(tmp_dir):
             # TODO: get which file we care
-            print(get_timestamp_in_minutes_and_seconds(filename, frames_per_second))
+            timestamp_in_string = get_timestamp_in_minutes_and_seconds(filename, frames_per_second)
+
+            # TODO: Upload video
+            '''
+            insert_timestamp_to_video(api_client,
+                                      {'snippet.channelId': channel_id,
+                                       'snippet.videoId': video_id,
+                                       'snippet.topLevelComment.snippet.textOriginal': timestamp_in_string},
+                                      part='snippet')
+            '''
+            break
 
 
 def get_and_download_video_local_location(video_id, new_videos_location):
@@ -39,3 +49,44 @@ def get_timestamp_in_minutes_and_seconds(image_chosen, frames_per_second):
         return f"{timestamp_minutes}:0{timestamp_seconds}"
 
     return f"{timestamp_minutes}:{timestamp_seconds}"
+
+
+def insert_timestamp_to_video(client, properties, **kwargs):
+    resource = build_resource(properties)
+
+    response = client.commentThreads().insert(
+        body=resource,
+        **kwargs
+    ).execute()
+
+    print(response)
+
+    # TODO: Check if successfully
+
+
+def build_resource(properties):
+    resource = {}
+    for p in properties:
+        prop_array = p.split('.')
+        ref = resource
+        for pa in range(0, len(prop_array)):
+            is_array = False
+            key = prop_array[pa]
+
+            if key[-2:] == '[]':
+                key = key[0:len(key) - 2:]
+                is_array = True
+
+            if pa == (len(prop_array) - 1):
+                if properties[p]:
+                    if is_array:
+                        ref[key] = properties[p].split(',')
+                    else:
+                        ref[key] = properties[p]
+            elif key not in ref:
+                ref[key] = {}
+                ref = ref[key]
+            else:
+                ref = ref[key]
+    return resource
+
