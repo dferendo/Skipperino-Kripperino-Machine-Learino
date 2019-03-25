f1bb46bedb35e5ce857f8831964a1a3cf6fb92fb
Ferendo <dylanfrendo@gmail.com>
2019-03-25 23:45:22 +0100

    Editing the label file

--- TrainBot/LabelImage.py
+++ TrainBot/LabelImage.py
@@ -73,10 +73,9 @@ def load_labels(label_file):
 
 
 if __name__ == "__main__":
-    file_name = "tensorflow/examples/label_image/data/grace_hopper.jpg"
-    model_file = \
-        "tensorflow/examples/label_image/data/inception_v3_2016_08_28_frozen.pb"
-    label_file = "tensorflow/examples/label_image/data/imagenet_slim_labels.txt"
+    file_name = ""
+    model_file = ""
+    label_file = ""
     input_height = 299
     input_width = 299
     input_mean = 0
